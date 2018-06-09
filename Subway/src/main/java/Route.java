ae5f387633ea2b010ca73d67060cae711fa23ca9
pbzweihander <sd852456@naver.com>
2018-06-09 16:30:25 +0900

    data processing

--- Subway/src/main/java/Route.java
+++ Subway/src/main/java/Route.java
@@ -1,3 +1,16 @@
-public class Route {
+import java.util.List;
 
+public interface Route extends List<Station> {
+    public static String toStringOf(Route r) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < r.size(); i++) {
+            Station s = r.get(i);
+            if (i + 1 < r.size() && r.get(i + 1).getName().equals(s.getName())) {
+                builder.append("[" + s.getName() + "] ");
+                i++;
+            } else
+                builder.append(s.getName() + " ");
+        }
+        return builder.toString().trim();
+    }
 }
--- Subway/src/main/java/Subway.java
+++ Subway/src/main/java/Subway.java
@@ -5,20 +5,70 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Subway {
-    public Subway(Reader reader) {
+    private static final Pattern inputPattern = Pattern.compile("^(\\S+?) (\\S+?)( !)?$");
+    private static final Pattern stationPattern = Pattern.compile("^(\\S+?) (\\S+?) (\\S+?)$");
+    private static final Pattern edgePattern = Pattern.compile("^(\\S+?) (\\S+?) (\\d+?)$");
+
+    private HashMap<String, List<Station>> nameToStationsMap;
+    private HashMap<String, Station> codeToStationMap;
+
+    public Subway(Reader reader) throws IOException {
+        nameToStationsMap = new HashMap<>();
+        codeToStationMap = new HashMap<>();
+
         BufferedReader bufferedReader = new BufferedReader(reader);
+
+        String inputLine;
+        while ((inputLine = bufferedReader.readLine()) != null && !inputLine.isEmpty()) {
+            Matcher matcher = stationPattern.matcher(inputLine);
+
+            String code = matcher.group(1);
+            String name = matcher.group(2);
+            String line = matcher.group(3);
+
+            if (nameToStationsMap.containsKey(name)) {
+                Station station = new Station(code, name, line);
+                codeToStationMap.put(code, station);
+                List<Station> stations = nameToStationsMap.get(name);
+                for (Station s : stations) {
+                    s.addEdge(station, 1, 0);
+                    station.addEdge(s, 1, 0);
+                }
+            } else {
+                Station station = new Station(code, name, line);
+                List<Station> stations = new ArrayList<>();
+                stations.add(station);
+                nameToStationsMap.put(name, stations);
+                codeToStationMap.put(code, station);
+            }
+        }
+
+        while ((inputLine = bufferedReader.readLine()) != null) {
+            Matcher matcher = edgePattern.matcher(inputLine);
+
+            String from = matcher.group(1);
+            String to = matcher.group(2);
+            Integer weight = Integer.getInteger(matcher.group(3));
+
+            Station fromStation = codeToStationMap.get(from);
+            Station toStation = codeToStationMap.get(to);
+            fromStation.addEdge(toStation, 0, weight);
+        }
     }
 
     public Route getShortestRoute(String start, String end) {
-        return new Route();
+        return null;
     }
 
     public Route getMinimumTransiRoute(String start, String end) {
-        return new Route();
+        return null;
     }
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
@@ -32,8 +82,6 @@ public class Subway {
             processLine(subway, line);
     }
 
-    private static final Pattern inputPattern = Pattern.compile("^(\\S+?) (\\S+?)( !)?$");
-
     public static void processLine(Subway subway, String line) {
         Matcher matcher = inputPattern.matcher(line);
 
@@ -42,7 +90,7 @@ public class Subway {
             String station2 = matcher.group(2);
             String group3 = matcher.group(3);
 
-            Route route;
+            List<Station> route;
             if (group3 != null && !group3.isEmpty())
                 route = subway.getMinimumTransiRoute(station1, station2);
             else
